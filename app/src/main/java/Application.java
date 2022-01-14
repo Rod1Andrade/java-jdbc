6112aacbe66ea3fcdb3b130f5751f1486f20b5e7
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2022-01-14 15:00:42 -0300

    UPDATE - Gradle Migration

---
+++ app/src/main/java/Application.java
@@ -0,0 +1,13 @@
+/**
+ * Estudo do acesso a dados através da API
+ * JDBC.
+ *
+ * @author Rodrigo Andrade
+ * @since 01-14-2022
+ * @version 1.0
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
