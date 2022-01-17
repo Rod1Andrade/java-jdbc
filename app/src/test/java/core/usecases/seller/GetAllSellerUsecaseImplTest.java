5a9fc0731e8fd194e01100af26e07cc2ae755483
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2022-01-17 08:41:17 -0300

    UPDATE/TEST - Get all sellers

---
+++ app/src/test/java/core/usecases/seller/GetAllSellerUsecaseImplTest.java
@@ -0,0 +1,24 @@
+package core.usecases.seller;
+
+import core.entities.Seller;
+import core.repositories.SellerRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetAllSellerUsecaseImplTest {
+
+    @Test
+    public void getAllSellerTest() {
+        List<Seller> sellers = new ArrayList<>();
+        SellerRepository repository = Mockito.mock(SellerRepository.class);
+        Mockito.when(repository.getAll()).thenReturn(sellers);
+
+        GetAllSellerUsecase usecase = new GetAllSellerUsecaseImpl(repository);
+        assertIterableEquals(sellers, usecase.apply());
+    }
+}
